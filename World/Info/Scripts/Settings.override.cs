0fb20de1b6111aab2c12d7cd6ce884413185ef63
justastrangerlica <justastrangerlica@gmail.com>
2025-04-28 22:32:35 -0500

    Add settings override

--- World/Info/Scripts/Settings.override.cs
+++ World/Info/Scripts/Settings.override.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Server
 {
 	/// <summary>
@@ -12,4 +13,25 @@ namespace Server
 			// MySettings.S_SaveOnCharacterLogout = true;
 		}
 	}
+    /// <summary>
+    /// You may apply explicit overrides `MySettings` overrides here.
+    /// This file will never be touched by Memento development.
+    /// </summary>
+    public static class SettingOverrides
+    {
+        public static void Initialize()
+        {
+            // Example:
+            // MySettings.S_SaveOnCharacterLogout = true;
+            MySettings.S_ServerSaveMinutes = 15.0;
+            MySettings.S_MaxMerchant = 4000;
+            MySettings.S_BlackMarket = true;
+            //MySettings.S_CustomMerchant = true;
+            MySettings.S_HousesPerAccount = -1;
+            MySettings.S_HouseStorage = true;
+            MySettings.S_DamageToPets = 1.2;
+            MySettings.S_Stables = 7;
+            Console.WriteLine("Settings Overridden.");
+        }
+    }
 }
