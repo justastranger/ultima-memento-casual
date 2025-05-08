27b811801ecde7208186c7c67d062e3a4303c96d
justastrangerlica <justastrangerlica@gmail.com>
2025-05-08 10:09:25 -0500

    Lower permission requirements for a couple commands to Counselor Two info commands (weather, world) and two for RichVein regeneration

--- World/Source/Scripts/System/Regions/Core/World.cs
+++ World/Source/Scripts/System/Regions/Core/World.cs
@@ -1656,7 +1656,7 @@ namespace Server.Misc
     {
 		public static void Initialize()
 		{
-            CommandSystem.Register( "world", AccessLevel.Administrator, new CommandEventHandler( WhereWorld_OnCommand ) );
+            CommandSystem.Register( "world", AccessLevel.Player, new CommandEventHandler( WhereWorld_OnCommand ) );
 		}
 		public static void Register( string command, AccessLevel access, CommandEventHandler handler )
 		{
