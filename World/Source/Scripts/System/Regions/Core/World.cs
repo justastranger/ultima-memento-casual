f193b8f07fa1d32271dec5a27f2ac987f065c405
justastrangerlica <justastrangerlica@gmail.com>
2025-05-10 15:07:47 -0500

    Allow teleporting to another island exterior where the interior is blocked, Island of the Black Knight

--- World/Source/Scripts/System/Regions/Core/World.cs
+++ World/Source/Scripts/System/Regions/Core/World.cs
@@ -565,8 +565,9 @@ namespace Server.Misc
 			if ( reg.IsPartOf( "the Painting of the Glade" ) )
 				return false;
 
-			if ( reg.IsPartOf( "the Island of the Black Knight" ) )
-				return false;
+			if (reg.IsPartOf("the Island of the Black Knight"))
+                //return false;
+                return true;
 
 			if ( reg.IsPartOf( "the Castle of the Black Knight" ) )
 				return false;
