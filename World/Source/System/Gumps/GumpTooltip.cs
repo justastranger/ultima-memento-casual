d578fe09f5ec999cf0694c50048121e473d32f03
justastrangerlica <justastrangerlica@gmail.com>
2025-05-16 11:58:42 -0500

    Format Documents with VS's Code Cleanup tool

--- World/Source/System/Gumps/GumpTooltip.cs
+++ World/Source/System/Gumps/GumpTooltip.cs
@@ -23,73 +23,73 @@ using Server.Network;
 
 namespace Server.Gumps
 {
-	/// <summary>
-	///  Adds that hover over tooltip to the previous Gump object, it can be text or an image, or button, item etc
-	///  You can also do line breaks on tooltips by adding "\n" or "<br>" to long strings
-	/// </summary>
-	public class GumpTooltip : GumpEntry
-	{
-		private int m_Number;
-		private string m_Args;
+    /// <summary>
+    ///  Adds that hover over tooltip to the previous Gump object, it can be text or an image, or button, item etc
+    ///  You can also do line breaks on tooltips by adding "\n" or "<br>" to long strings
+    /// </summary>
+    public class GumpTooltip : GumpEntry
+    {
+        private int m_Number;
+        private string m_Args;
 
-		public GumpTooltip( int number )
-		{
-			m_Number = number;
-		}
+        public GumpTooltip(int number)
+        {
+            m_Number = number;
+        }
 
-		public GumpTooltip(string args)
-		{
-			m_Number = 1114057; // ~1_val~
-			m_Args = args;
-		}
+        public GumpTooltip(string args)
+        {
+            m_Number = 1114057; // ~1_val~
+            m_Args = args;
+        }
 
-		public int Number
-		{
-			get
-			{
-				return m_Number;
-			}
-			set
-			{
-				Delta( ref m_Number, value );
-			}
-		}
+        public int Number
+        {
+            get
+            {
+                return m_Number;
+            }
+            set
+            {
+                Delta(ref m_Number, value);
+            }
+        }
 
-		public string Args
-		{
-			get
-			{
-				return m_Args;
-			}
-			set
-			{
-				Delta(ref m_Args, value);
-			}
-		}
+        public string Args
+        {
+            get
+            {
+                return m_Args;
+            }
+            set
+            {
+                Delta(ref m_Args, value);
+            }
+        }
 
-		public override string Compile()
-		{
-			if (m_Args != null)
-			{
-				var args = m_Args.Replace("@", "\uFF20").Replace("\n", "<br>");
+        public override string Compile()
+        {
+            if (m_Args != null)
+            {
+                var args = m_Args.Replace("@", "\uFF20").Replace("\n", "<br>");
 
-				return String.Format("{{ tooltip {0} @{1}@ }}", m_Number, args);
-			}
-			return String.Format( "{{ tooltip {0} }}", m_Number );
-		}
+                return String.Format("{{ tooltip {0} @{1}@ }}", m_Number, args);
+            }
+            return String.Format("{{ tooltip {0} }}", m_Number);
+        }
 
-		private static byte[] m_LayoutName = Gump.StringToBuffer( "tooltip" );
+        private static byte[] m_LayoutName = Gump.StringToBuffer("tooltip");
 
-		public override void AppendTo( IGumpWriter disp )
-		{
-			disp.AppendLayout( m_LayoutName );
-			disp.AppendLayout( m_Number );
-			if (m_Args != null)
-			{
-				var args = m_Args.Replace("@", "\uFF20").Replace("\n", "<br>");
+        public override void AppendTo(IGumpWriter disp)
+        {
+            disp.AppendLayout(m_LayoutName);
+            disp.AppendLayout(m_Number);
+            if (m_Args != null)
+            {
+                var args = m_Args.Replace("@", "\uFF20").Replace("\n", "<br>");
 
-				disp.AppendLayout(args);
-			}
-		}
-	}
+                disp.AppendLayout(args);
+            }
+        }
+    }
 }
