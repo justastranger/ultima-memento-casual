3e5f7ec291d6a35d7d832ff5245771f5c698db5b
justastrangerlica <justastrangerlica@gmail.com>
2025-05-16 11:52:33 -0500

    Ditto for axes and hatchets for carpenter Additonally, enabled automatic document formatting through Visual Studio so everything's going to get cleaned up from now on

--- World/Source/Scripts/Mobiles/Civilized/Merchants/Carpenter.cs
+++ World/Source/Scripts/Mobiles/Civilized/Merchants/Carpenter.cs
@@ -3,95 +3,99 @@ using Server.Items;
 
 namespace Server.Mobiles
 {
-	public class Carpenter : BaseVendor
-	{
-		private List<SBInfo> m_SBInfos = new List<SBInfo>();
-		protected override List<SBInfo> SBInfos{ get { return m_SBInfos; } }
-		public override bool IsBlackMarket { get { return true; } }
+    public class Carpenter : BaseVendor
+    {
+        private List<SBInfo> m_SBInfos = new List<SBInfo>();
+        protected override List<SBInfo> SBInfos { get { return m_SBInfos; } }
+        public override bool IsBlackMarket { get { return true; } }
 
-		public override NpcGuild NpcGuild{ get{ return NpcGuild.CarpentersGuild; } }
+        public override NpcGuild NpcGuild { get { return NpcGuild.CarpentersGuild; } }
 
-		[Constructable]
-		public Carpenter() : base( "the carpenter" )
-		{
-			SetSkill( SkillName.Carpentry, 85.0, 100.0 );
-			SetSkill( SkillName.Lumberjacking, 60.0, 83.0 );
-		}
+        [Constructable]
+        public Carpenter() : base("the carpenter")
+        {
+            SetSkill(SkillName.Carpentry, 85.0, 100.0);
+            SetSkill(SkillName.Lumberjacking, 60.0, 83.0);
+        }
 
-		public override void InitSBInfo( Mobile m )
-		{
-			m_Merchant = m;
-			m_SBInfos.Add( new MyStock() );
-		}
+        public override void InitSBInfo(Mobile m)
+        {
+            m_Merchant = m;
+            m_SBInfos.Add(new MyStock());
+        }
 
-		public class MyStock: SBInfo
-		{
-			private List<GenericBuyInfo> m_BuyInfo = new InternalBuyInfo();
-			private IShopSellInfo m_SellInfo = new InternalSellInfo();
+        public class MyStock : SBInfo
+        {
+            private List<GenericBuyInfo> m_BuyInfo = new InternalBuyInfo();
+            private IShopSellInfo m_SellInfo = new InternalSellInfo();
 
-			public MyStock()
-			{
-			}
+            public MyStock()
+            {
+            }
 
-			public override IShopSellInfo SellInfo { get { return m_SellInfo; } }
-			public override List<GenericBuyInfo> BuyInfo { get { return m_BuyInfo; } }
+            public override IShopSellInfo SellInfo { get { return m_SellInfo; } }
+            public override List<GenericBuyInfo> BuyInfo { get { return m_BuyInfo; } }
 
-			public class InternalBuyInfo : List<GenericBuyInfo>
-			{
-				public InternalBuyInfo()
-				{
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.None,		ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.Armor,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.Shield,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.Weapon,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.Resource,	ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetSellList( m_Merchant, this, 	ItemSalesInfo.Category.Rare,		ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-				}
-			}
+            public class InternalBuyInfo : List<GenericBuyInfo>
+            {
+                public InternalBuyInfo()
+                {
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.None, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Armor, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Shield, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Weapon, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Resource, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Weapon, ItemSalesInfo.Material.Metal, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetSellList(m_Merchant, this, ItemSalesInfo.Category.Rare, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                }
+            }
 
-			public class InternalSellInfo : GenericSellInfo
-			{
-				public InternalSellInfo()
-				{
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.None,		ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.Armor,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.Shield,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.Weapon,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,			ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.Resource,	ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-					ItemInformation.GetBuysList( m_Merchant, this, 	ItemSalesInfo.Category.Rare,		ItemSalesInfo.Material.None,		ItemSalesInfo.Market.Carpenter,		ItemSalesInfo.World.None,	null	 );
-				}
-			}
-		}
+            public class InternalSellInfo : GenericSellInfo
+            {
+                public InternalSellInfo()
+                {
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.None, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Armor, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Shield, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Weapon, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Resource, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Weapon, ItemSalesInfo.Material.Metal, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                    ItemInformation.GetBuysList(m_Merchant, this, ItemSalesInfo.Category.Rare, ItemSalesInfo.Material.None, ItemSalesInfo.Market.Carpenter, ItemSalesInfo.World.None, null);
+                }
+            }
+        }
 
-		public override void UpdateBlackMarket()
-		{
-			base.UpdateBlackMarket();
+        public override void UpdateBlackMarket()
+        {
+            base.UpdateBlackMarket();
 
-			if ( IsBlackMarket && MyServerSettings.BlackMarket() )
-			{
-				int v=4; while ( v > 0 ){ v--;
-				ItemInformation.BlackMarketList( this, ItemSalesInfo.Category.Armor,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,		ItemSalesInfo.World.None	 );
-				ItemInformation.BlackMarketList( this, ItemSalesInfo.Category.Shield,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,		ItemSalesInfo.World.None	 );
-				ItemInformation.BlackMarketList( this, ItemSalesInfo.Category.Weapon,		ItemSalesInfo.Material.Wood,		ItemSalesInfo.Market.All,		ItemSalesInfo.World.None	 );
-				ItemInformation.BlackMarketList( this, ItemSalesInfo.Category.All,			ItemSalesInfo.Material.All,			ItemSalesInfo.Market.All,		ItemSalesInfo.World.None,	typeof( TenFootPole )	 );
-				}
-			}
-		}
+            if (IsBlackMarket && MyServerSettings.BlackMarket())
+            {
+                int v = 4; while (v > 0)
+                {
+                    v--;
+                    ItemInformation.BlackMarketList(this, ItemSalesInfo.Category.Armor, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None);
+                    ItemInformation.BlackMarketList(this, ItemSalesInfo.Category.Shield, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None);
+                    ItemInformation.BlackMarketList(this, ItemSalesInfo.Category.Weapon, ItemSalesInfo.Material.Wood, ItemSalesInfo.Market.All, ItemSalesInfo.World.None);
+                    ItemInformation.BlackMarketList(this, ItemSalesInfo.Category.All, ItemSalesInfo.Material.All, ItemSalesInfo.Market.All, ItemSalesInfo.World.None, typeof(TenFootPole));
+                }
+            }
+        }
 
-		public Carpenter( Serial serial ) : base( serial )
-		{
-		}
+        public Carpenter(Serial serial) : base(serial)
+        {
+        }
 
-		public override void Serialize( GenericWriter writer )
-		{
-			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
-		}
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+        }
 
-		public override void Deserialize( GenericReader reader )
-		{
-			base.Deserialize( reader );
-			int version = reader.ReadInt();
-		}
-	}
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+    }
 }
